1d55e66f6747dae6a51109326a22416ffb067b39
Nickeled <132020934+Nickeled@users.noreply.github.com>
2025-01-13 16:35:07 -0500

    Minor performance optimizations

--- pose.py
+++ pose.py
@@ -9,6 +9,9 @@ mp_drawing = mp.solutions.drawing_utils
 
 cap = cv.VideoCapture(0)
 
+cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+
 if not cap.isOpened():
     print("Cannot open camera. Exiting...")
     exit()
@@ -22,12 +25,13 @@ while True:
 
     image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
     result = pose.process(image)
+    image = cv.cvtColor(image, cv.COLOR_RGB2BGR) # Convert back for display
     if result.pose_landmarks:
         mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-    cv.imshow('Pose Detection', frame)
+    cv.imshow('Pose Detection', frame) # Remove cv.imshow and frame rendering to improve performance
 
-    if cv.waitKey(10) == ord('q'):
+    if cv.waitKey(50) == ord('q'):
         break
     
 cv.destroyAllWindows()
