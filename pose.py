757fad8a9ce0eab35ceba84d736d654f8f9bce38
Nickeled <132020934+Nickeled@users.noreply.github.com>
2025-01-14 03:35:42 -0500

    Updated resolution

--- pose.py
+++ pose.py
@@ -11,7 +11,7 @@ mp_drawing = mp.solutions.drawing_utils
 
 # Initialize the Pi Camera
 picam2 = Picamera2()
-config = picam2.create_preview_configuration(main={"size": (640, 480)})
+config = picam2.create_preview_configuration(main={"size": (854, 480)})
 picam2.configure(config)
 picam2.start()
 
