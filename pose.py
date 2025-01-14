da681f0da48f3ba2185df84164e7828df7709b47
Nickeled <132020934+Nickeled@users.noreply.github.com>
2025-01-14 13:37:46 -0500

    updated resolution and rotation

--- pose.py
+++ pose.py
@@ -12,7 +12,7 @@ mp_drawing = mp.solutions.drawing_utils
 
 # Initialize the Pi Camera
 picam2 = Picamera2()
-config = picam2.create_preview_configuration(main={"size": (640, 480)})
+config = picam2.create_preview_configuration(main={"size": (854, 480)})
 picam2.configure(config)
 picam2.start()
 
@@ -25,7 +25,7 @@ while True:
     frame = picam2.capture_array()
 
     # Rotate the frame 90 degrees clockwise
-    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
     # Convert the frame to RGB for MediaPipe
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
