7082047e4c2773bdde332a993b8c77b2ec8ee829
Nickeled <132020934+Nickeled@users.noreply.github.com>
2025-01-17 21:39:39 -0500

    fix sounds

--- sounds.py
+++ sounds.py
@@ -4,15 +4,15 @@ GPIO.setmode(GPIO.BOARD)
 
 def playWhackamole():
     trigger_pin = 23
-    GPIO.setup(trigger_pin, GPIO.OUT)
-    GPIO.output(trigger_pin, GPIO.LOW)
+    GPIO.setup(trigger_pin, GPIO.OUT, initial=GPIO.HIGH)  # Set initial state to HIGH
+    GPIO.output(trigger_pin, GPIO.LOW)  # Trigger active low
     time.sleep(0.1)  # Trigger for 100ms
-    GPIO.output(trigger_pin, GPIO.HIGH)
-    GPIO.cleanup(trigger_pin)
+    GPIO.output(trigger_pin, GPIO.HIGH)  # Reset to inactive
+    GPIO.cleanup(trigger_pin)  # Clean up after use
 
 def playMemory():
     trigger_pin = 27
-    GPIO.setup(trigger_pin, GPIO.OUT)
+    GPIO.setup(trigger_pin, GPIO.OUT, initial=GPIO.HIGH)
     GPIO.output(trigger_pin, GPIO.LOW)
     time.sleep(0.1)
     GPIO.output(trigger_pin, GPIO.HIGH)
@@ -20,7 +20,7 @@ def playMemory():
 
 def playShake():
     trigger_pin = 21
-    GPIO.setup(trigger_pin, GPIO.OUT)
+    GPIO.setup(trigger_pin, GPIO.OUT, initial=GPIO.HIGH)
     GPIO.output(trigger_pin, GPIO.LOW)
     time.sleep(0.1)
     GPIO.output(trigger_pin, GPIO.HIGH)
@@ -28,7 +28,7 @@ def playShake():
 
 def playJacks():
     trigger_pin = 19
-    GPIO.setup(trigger_pin, GPIO.OUT)
+    GPIO.setup(trigger_pin, GPIO.OUT, initial=GPIO.HIGH)
     GPIO.output(trigger_pin, GPIO.LOW)
     time.sleep(0.1)
     GPIO.output(trigger_pin, GPIO.HIGH)
@@ -36,7 +36,7 @@ def playJacks():
 
 def playMenu():
     trigger_pin = 15
-    GPIO.setup(trigger_pin, GPIO.OUT)
+    GPIO.setup(trigger_pin, GPIO.OUT, initial=GPIO.HIGH)
     GPIO.output(trigger_pin, GPIO.LOW)
     time.sleep(0.1)
     GPIO.output(trigger_pin, GPIO.HIGH)
