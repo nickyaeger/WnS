34f6de3d303c2be0ac619e22c6976ae964ae2369
Serena Shih <sshih10@gmail.com>
2025-01-19 12:08:22 -0500

    sounds

--- sounds.py
+++ sounds.py
@@ -29,7 +29,7 @@ def playMenu():
     time.sleep(menu.get_length())
 
 def playAlarm():
-    alarm = pygame.mixer.Sound('/home/wns/WnS/audio/alarm.wav')
+    alarm = pygame.mixer.Sound('/home/wns/WnS/audio/repeat.wav')
     alarm.play()
     time.sleep(alarm.get_length())
 
@@ -37,3 +37,53 @@ def playPushup():
     pushup = pygame.mixer.Sound('/home/wns/WnS/audio/pushup.wav')
     pushup.play()
     time.sleep(pushup.get_length())
+
+def playOne():
+    one = pygame.mixer.Sound('/home/wns/WnS/audio/one.wav')
+    one.play()
+    time.sleep(one.get_length())
+
+def playTwo():
+    two = pygame.mixer.Sound('/home/wns/WnS/audio/two.wav')
+    two.play()
+    time.sleep(two.get_length())
+
+def playThree():
+    three = pygame.mixer.Sound('/home/wns/WnS/audio/three.wav')
+    three.play()
+    time.sleep(three.get_length())
+
+def playFour():
+    four = pygame.mixer.Sound('/home/wns/WnS/audio/four.wav')
+    four.play()
+    time.sleep(four.get_length())
+
+def playFive():
+    five = pygame.mixer.Sound('/home/wns/WnS/audio/five.wav')
+    five.play()
+    time.sleep(five.get_length())
+
+def playSix():
+    six = pygame.mixer.Sound('/home/wns/WnS/audio/six.wav')
+    six.play()
+    time.sleep(six.get_length())
+
+def playSeven():
+    seven = pygame.mixer.Sound('/home/wns/WnS/audio/seven.wav')
+    seven.play()
+    time.sleep(seven.get_length())
+
+def playEight():
+    eight = pygame.mixer.Sound('/home/wns/WnS/audio/eight.wav')
+    eight.play()
+    time.sleep(eight.get_length())
+
+def playNine():
+    nine = pygame.mixer.Sound('/home/wns/WnS/audio/nine.wav')
+    nine.play()
+    time.sleep(nine.get_length())
+
+def playTen():
+    ten = pygame.mixer.Sound('/home/wns/WnS/audio/ten.wav')
+    ten.play()
+    time.sleep(ten.get_length())
